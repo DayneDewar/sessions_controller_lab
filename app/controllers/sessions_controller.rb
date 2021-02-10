419bb419c9a2446f54be712ee6da523162ac22f3
Dayne Dewar <daynedewar@yahoo.com>
2021-02-10 15:23:11 -0500

    commit tree 20540acdaaf60013e1d4aa724d76f67370688c17

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+    
+    def new
+    end
+
+    def create
+        session[:name] = params[:name]
+        
+        if params[:name] == nil || params[:name].empty?
+            redirect_to '/login'
+        else
+            redirect_to '/'
+        end
+
+    end
+
+    
+    def destroy
+        session.destroy
+        redirect_to '/login'
+    end
+
+end
